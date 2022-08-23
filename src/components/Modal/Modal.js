19bc4d7abf8f42f5ee0f3ae56da7831104ccc4c4
Max-Loktionov <studreno@gmail.com>
2022-08-23 21:22:42 +0300

    delete other ways to close modal ChangeContact

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,22 +5,6 @@ import { Overlay, Box, ButtonClose } from './Modal.styled';
 const modalRoot = document.querySelector('#modal-root');
 
 const Modal = ({ children, onClose }) => {
-  useEffect(() => {
-    const handleEscapeKey = e => {
-      if (e.code === 'Escape') {
-        onClose();
-      }
-    };
-    document.addEventListener('keydown', handleEscapeKey);
-    return () => document.removeEventListener('keydown', handleEscapeKey);
-  });
-
-  const onBackdrop = e => {
-    if (e.currentTarget === e.target) {
-      onClose();
-    }
-  };
-
   return createPortal(
     <Overlay onClick={onBackdrop}>
       <Box>
