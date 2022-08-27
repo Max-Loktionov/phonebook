07e7c1acf16772ce5b7af937fbe984834684c7cb
Max-Loktionov <studreno@gmail.com>
2022-08-27 11:04:02 +0300

    fix style AppBar

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const Container = styled.div`
-  max-width: 960px;
   margin: 0 auto;
   padding: 0 16px;
 `;
@@ -11,10 +10,11 @@ export const Header = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-bottom: 1px solid #2a363b;
   width: 1200px;
   padding: 8px 0;
   margin-bottom: 16px;
+  box-shadow: rgb(6 24 44 / 40%) -1px 2px 0px -1px,
+    rgb(6 24 44 / 65%) 0px 1px 2px -1px, rgb(255 255 255 / 8%) 0px 1px 0px inset;
 `;
 
 export const NAVLink = styled(NavLink)`
